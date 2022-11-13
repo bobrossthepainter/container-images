78ce81c6ca8aa222d0f8815779e14465b0bc1d31
Rob Lang <dev@roblang.de>
2022-11-13 18:51:19 +0100

    fixes stuff

--- base/workbench-ubuntu-jammy/latest-version.sh
+++ base/workbench-ubuntu-jammy/latest-version.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
-version=$(curl -s "https://registry.hub.docker.com/v1/repositories/library/ubuntu/tags" | jq --raw-output '.[] | select(.name | contains("jammy")) | .name'  | tail -n1)
+item=library/ubuntu
+
+tokenUri="https://auth.docker.io/token"
+data=("service=registry.docker.io" "scope=repository:$item:pull")
+token="$(curl --silent --get --data-urlencode ${data[0]} --data-urlencode ${data[1]} $tokenUri | jq --raw-output '.token')"
+listUri="https://registry-1.docker.io/v2/$item/tags/list"
+authz="Authorization: Bearer $token"
+# result="$(curl --silent --get -H "Accept: application/json" -H "Authorization: Bearer $token" $listUri | jq --raw-output '.')"
+# echo $result
+
+
+version=$(curl -s -H "Authorization: Bearer $token" $listUri | jq --raw-output ' .tags[] | select( contains("jammy") )' | tail -n1)
 version="${version#*v}"
 version="${version#*release-}"
 printf "%s" "${version}"
