1979bacc11b5dc1508cec21baa0ce76cedb0a44a
Rob Lang <dev@roblang.de>
2023-02-24 13:51:36 +0100

    fix bash

--- base/workbench-ubuntu/insertions/bash_config.sh
+++ base/workbench-ubuntu/insertions/bash_config.sh
@@ -3,7 +3,7 @@ git_branch(){
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/(\1)/'
 }
 
-source /etc/profile
+# source /etc/profile
 
 PS1='\[\033[01;32m\]\u@docker\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
 ### END - Ubuntu Jammy insertions ######
