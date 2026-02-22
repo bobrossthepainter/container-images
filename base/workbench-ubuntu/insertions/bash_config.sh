851f924c533ac0b99836aaf2d269e4e56b1a349c
Rob Lang <dev@roblang.de>
2026-02-22 12:41:33 +0100

    fixes terminal issue + improived intellij performance

--- base/workbench-ubuntu/insertions/bash_config.sh
+++ base/workbench-ubuntu/insertions/bash_config.sh
@@ -5,5 +5,11 @@ git_branch(){
 
 # source /etc/profile
 
-PS1='\[\033[01;32m\]\u@docker\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+if [ -n "$ZSH_VERSION" ]; then
+    # zsh: use %{...%} to mark non-printing sequences, and zsh prompt escapes
+    PROMPT='%{%F{green}%B%}%n@docker%{%b%f%}:%{%F{blue}%B%}%~%{%b%f%}%# '
+elif [ -n "$BASH_VERSION" ]; then
+    # bash: use \[...\] to mark non-printing sequences
+    PS1='\[\033[01;32m\]\u@docker\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+fi
 ### END - Ubuntu Jammy insertions ######
